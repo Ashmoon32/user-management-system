563ecf2d6c1e463fe11c900e3c5dd4471b523641
Ashmoon32 <nyeinmaung3021@gmail.com>
2026-04-28 10:19:56 +0630

    -

--- project/index.php
+++ project/index.php
@@ -18,11 +18,25 @@
     <div class="wrap">
         <h1 class="h3 mb-3">Login</h1>
         <?php
+        if (isset($_GET['registered'])): ?>
+            <div class="alert alert-success">
+                Account created. Please login.
+            </div>
+        <?php endif ?>
+
+        <?php if (isset($_GET['suspended'])): ?>
+            <div class="alert alert-danger">
+                Your account is suspended.
+            </div>
+        <?php endif ?>
+
+        <?php
         if (isset($_GET['incorrect'])): ?>
             <div class="alert alert-warning">
                 Incorrect email or password
             </div>
         <?php endif ?>
+
         <form action="_actions/login.php" method="post">
             <input type="email" name="email" class="form-control mb-2" placeholder="Email" required>
             <input type="password" name="password" class="form-control mb-2" placeholder="Password" required>
